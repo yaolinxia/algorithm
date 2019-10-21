32ee77e8d6afba2c34e8a9ded23f789fcf011066
yaolinxia <18860976931@163.com>
2019-10-21 10:09:31 +0800

    Update a常用函数python汇总.py

--- a常用函数python汇总.py
+++ a常用函数python汇总.py
@@ -18,7 +18,16 @@ def str2list(s="[2,3,4,1]"):
 def sorted_dict(d={"a":3, "f":5, "b":6}):
     s_d = sorted(d, key=lambda i:d[i])
     print(s_d)
+# 按照字符串的第一个字母进行对整个字符串对排序
+def sorted_s(l=["delphi" ,"Delphi" ,"python" ,"Python" ,"c++" ,"C++" ,"c" ,"C" ,"golang" ,"Golang"]):
+    l.sort(key=lambda s: s[0])
+    print(l)
 
+def test():
+    from functools import reduce
+    def fn(m,n):
+        return m*10+n
+    print(reduce(fn, [8,9,5,6,3,2]))
 
 
 if __name__ == '__main__':
